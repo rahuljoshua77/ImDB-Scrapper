9df445b40a8b5a5adf0bf0c3a8982ed54f6033c6
Rahul Joshua Damanik <73378179+rahuljoshua77@users.noreply.github.com>
2022-11-26 14:42:32 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,46 @@
+# https://www.imdb.com/title/tt9114286/
+from requests_html import HTMLSession
+import time, csv
+session = HTMLSession()
+
+def date():
+    date = f"[{time.strftime('%d-%m-%y %X')}]"
+    return date
+
+def scrape(response):
+    title = response.html.xpath('//h1/text()')[0]
+    year = response.html.xpath('(//h1/following-sibling::div/ul/li/a)[1]/text()')[0]
+    description = response.html.xpath("//span[@data-testid='plot-l']/text()")[0]
+    url_poster = "https://www.imdb.com"+response.html.xpath('(//a[@class="ipc-lockup-overlay ipc-focusable"])[1]')[0].attrs['href']
+    id = url_poster.split("/title/")[1].split('/mediaviewer/')[0]
+    return dict({'title':title,
+                 'year':year,
+                 'description':description,
+                 'url_poster':url_poster,
+                 'id':id
+                })
+
+def main(i):
+    response = session.get(i)
+    dict_data = scrape(response)
+    print(f'{date()} [ID: {dict_data["id"]} ] Successfully scrapped ')
+    with open(save_file, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=['title','year','description','url_poster','id'])
+        writer.writeheader()
+        for data in [dict_data]:
+            writer.writerow(data)
+
+
+if __name__ == '__main__': 
+    global save_file
+    print(f'{date()} ImDB Scrapper')
+    datas = input(f'{date()} Folder ID/URL: ')
+    save_file = input(f'{date()} File to save (csv): ')
+    data = f"{datas}"
+    file = open(f"./{data}","r")
+    list_ids = file.read()
+    list_id = list_ids.split("\n")
+    for i in list_id:
+        if "https://www.imdb.com" not in i:
+            i = "https://www.imdb.com/title/"+i
+        main(i)
